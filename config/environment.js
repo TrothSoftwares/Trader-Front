f39f4bf526bd4a4a50505dadd97a4034974cd7db
anvrnazr <anvrnazr@gmail.com>
2016-09-09 10:57:20 +0530

    after changing backend url

--- config/environment.js
+++ config/environment.js
@@ -63,7 +63,7 @@ ENV['ember-simple-auth'] = {
   }
 
   if (environment === 'test') {
-    ENV.APP.host =  'http://inventdemo-front.herokuapp.com';
+    ENV.APP.host =  'http://inventdemo-back.herokuapp.com';
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
@@ -76,7 +76,7 @@ ENV['ember-simple-auth'] = {
   }
 
   if (environment === 'production') {
-    ENV.APP.host =  'http://inventdemo-front.herokuapp.com';
+    ENV.APP.host =  'http://inventdemo-back.herokuapp.com';
 
   }
 //   if (deployTarget === 'staging') {
